fd383996f6225e08ffe0d19e79d46917a5495fa7
alistonbca <aliston25@outlook.com>
2021-04-07 07:22:40 +0530

    first commit

---
+++ src/students/bankaccount.java
@@ -0,0 +1,46 @@
+package students;
+
+public class bankaccount {
+	
+	int availablebalance,accountnumber;
+	String accountholdername,bankname;
+	
+	bankaccount(int availablebalance,int accountnumber,String accountholdername,String bankname)
+	{
+		this.availablebalance=availablebalance;
+		this.accountnumber=accountnumber;
+		this.accountholdername=	accountholdername;
+	    this.bankname=bankname;
+	}
+	
+	void deposit(int depo) 
+	{
+		int newbalance;
+		newbalance=availablebalance+depo;
+		availablebalance=newbalance;
+	}
+	
+	void withdraw(int amount)
+	{
+	int finalbalance;
+	finalbalance=availablebalance-amount;
+	availablebalance=finalbalance;
+	}
+	void display() 
+	{
+	System.out.println("bank name: "+bankname);
+	System.out.println("account holders name: "+accountholdername);
+	System.out.println("account holders number: "+accountnumber);
+	System.out.println("available balance: "+availablebalance);
+	
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		bankaccount aliston=new bankaccount(500000,12345,"aliston","state bank of india");
+				aliston.deposit(5000);
+				aliston.display();
+	}
+
+}
